d57b17945a458541502fa9203d96ec752e381ea8
Pedro Ramos <pedrotramos97@gmail.com>
2020-11-18 20:18:37 -0300

    Fix

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("listAll", views.get_tasks, name="TaskList"),
     path("<int:task_id>", views.get_task, name="Task"),
-    path("add", views.add_task name="AddTask"),
+    path("add", views.add_task, name="AddTask"),
     path("alter/<int:task_id>", views.alter_ask, name="AlterTask"),
     path("remove/<int:task_id>", views.remove_task, name="RemoveTask"),
 ]
