87473e3ae4ca033f586f66b7ae2c9f8352aa0e91
Pedro Ramos <pedrotramos97@gmail.com>
2020-12-03 01:32:26 -0300

    Fix urls

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("listAll", views.get_tasks, name="TaskList"),
+    path("", views.get_tasks, name="TaskList"),
     path("<int:task_id>", views.get_task, name="Task"),
-    path("add", views.add_task, name="AddTask"),
-    path("alter/<int:task_id>", views.alter_task, name="AlterTask"),
-    path("remove/<int:task_id>", views.remove_task, name="RemoveTask"),
+    path("", views.add_task, name="AddTask"),
+    path("<int:task_id>", views.alter_task, name="AlterTask"),
+    path("<int:task_id>", views.remove_task, name="RemoveTask"),
 ]
